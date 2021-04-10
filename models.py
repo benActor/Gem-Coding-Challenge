18f61da809ef6db5fa7fedb9eaf0104d27075323
benActor <210335@university-365.com>
2021-04-10 15:22:44 +0200

    power plant attributes added

--- models.py
+++ models.py
@@ -1,2 +1,9 @@
 class PowerPlant:
-    pass
+    def __init__(self, name, type, efficiency, pmin, pmax):
+        self.name, self.type, self.efficiency, self.pmin, self.pmax = name, type, efficiency, pmin, pmax
+
+    def __eq__(self, other):
+        if not isinstance(other, PowerPlant):
+            return NotImplementedError
+        else:
+            return self.name == other.name and self.type == other.type
