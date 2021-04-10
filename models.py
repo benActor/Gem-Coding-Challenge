87593d986d226441f67134b60fb18291ac88509c
benActor <210335@university-365.com>
2021-04-10 15:25:11 +0200

    DailyData class and test case added

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class PowerPlant:
     def __init__(self, name, type, efficiency, pmin, pmax):
         self.name, self.type, self.efficiency, self.pmin, self.pmax = name, type, efficiency, pmin, pmax
@@ -7,3 +9,13 @@ class PowerPlant:
             return NotImplementedError
         else:
             return self.name == other.name and self.type == other.type
+
+class DailyData:
+    def __init__(self, load, gas, kerosine, co2, wind):
+        self.load, self.gas, self.kerosine, self.co2, self.wind = load, gas, kerosine, co2, wind
+        self.__date = datetime.today().strftime('%Y-%m-%d')
+
+    @property
+    def date(self):
+        return self.__date
+
--- tests/test_powerplant.py
+++ tests/test_powerplant.py
@@ -1,10 +1,18 @@
 from unittest import TestCase
-from models import PowerPlant
+from models import PowerPlant, DailyData
 
 
 class PowerPlantTest(TestCase):
     def setUp(self) -> None:
-        self.powerPlant = PowerPlant()
+        self.powerPlant = PowerPlant("gasfiredbig1", "gasfired", 0.53, 100, 460)
 
     def test_canCreate_pwp(self):
         self.assertIsInstance(self.powerPlant, PowerPlant)
+
+
+class DailyDataTest(TestCase):
+    def setUp(self) -> None:
+        self.data = DailyData(480, 13.4, 50.8, 20, 60)
+
+    def test_date(self):
+        self.assertIsNotNone(self.data.date)
