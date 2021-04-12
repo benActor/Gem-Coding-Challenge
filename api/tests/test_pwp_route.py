d95ea64212131cbd1639bbeb87b61fb9f4a9cec6
benActor <210335@university-365.com>
2021-04-12 11:58:22 +0200

    importing create_app function in test

--- api/tests/test_pwp_route.py
+++ api/tests/test_pwp_route.py
@@ -1,22 +1,23 @@
 from unittest import TestCase
-from api import app
+from api import create_app
 import json
 
 
 class RouteTest(TestCase):
     def setUp(self) -> None:
 
-        self.app = app.test_client()
+        self.app_client = create_app().test_client()
+        self.app = create_app()
 
     def test_production_route(self):
         with open("payload.json") as f:
             data = json.load(f)
-        result = self.app.post("/productionplan", data=json.dumps(data), content_type='application/json')
+        result = self.app_client.post("/productionplan", data=json.dumps(data), content_type='application/json')
 
         self.assertEqual(result.status_code, 200)
 
     def test_running_port(self):
-        port = app.config["SERVER_NAME"].split(":")[1]
+        port = self.app.config["SERVER_NAME"].split(":")[1]
         self.assertEqual(port, "8888")
 
 
