0f9d6ce7c9e1f68428cd9e7d3346f644ef8459fb
benActor <210335@university-365.com>
2021-04-12 18:24:11 +0200

    socketio integrated to production route

--- api/power_plants/route.py
+++ api/power_plants/route.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify, Blueprint
 from api.power_plants.utils import PowerplantSvc
+from api.init_app import socketio
 
 production = Blueprint('production', __name__)
 
@@ -8,7 +9,11 @@ production = Blueprint('production', __name__)
 def production_plan():
     if request.is_json:
         pwp_svc = PowerplantSvc(request.json)
-        return jsonify([{"name": p.name, "p": pwp_svc.get_pwp_load(p)} for p in pwp_svc.get_optimal_combination()])
+        response = [{"name": p.name, "p": pwp_svc.get_pwp_load(p)} for p in pwp_svc.get_optimal_combination()]
+        socketio.emit("json", {"payload": request.json, "response": response})
+        return jsonify(response)
+
+
 
 
 
