d636db40a9bd4ba71eddf8fe10c7e0626182c543
benActor <210335@university-365.com>
2021-04-15 22:18:53 +0200

    payload parameter mapping corrected

--- api/tests/test_powerplant_svc.py
+++ api/tests/test_powerplant_svc.py
@@ -7,11 +7,12 @@ import json
 class PowerPlantSvcTest(TestCase):
     def setUp(self) -> None:
         with open("payload.json") as f:
-            self.payload = json.load(f)
+            self.test_data = json.load(f)
 
-        self.svc = PowerplantSvc(self.payload)
+        self.svc = PowerplantSvc(self.test_data["payload"])
 
     def test_can_load_pwp_from_json(self):
+
         self.assertIsInstance(self.svc.powerplants[0], PowerPlant)
 
         self.assertEqual(6, len(self.svc.powerplants))
